54928f499d26adeab678ac3c4cfe3ea70b37ea34
Srishty Bedi <70388131+srishty-07@users.noreply.github.com>
2022-08-23 03:20:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -84,7 +84,12 @@ function App() {
   return (
     <>
       <div className='max-w-5xl my-8 mx-auto'>
-        <MapContainer ref = {mapRef} center = {position} zoom={zoom} scrollWheelZoom = {true} style={{height:"60vh", width:"120vh"}} >
+        <h1 className='heading' style={{fontFamily: 'Rubik Maze',fontSize:"70px",textAlign:"center",margin:"25px"}}>
+            KNOW YOUR ROAD 🛣
+            
+        </h1>
+        <div style={{padding:"12px",background:"#283618", textAlign:"center",borderRadius:"12px"}}>
+        <MapContainer ref = {mapRef} center = {position} zoom={zoom} scrollWheelZoom = {true} style={{height:"60vh", width:"65vw"}} className="mapdesign">
           <LayersControl>
             <BaseLayer checked name="OpenStreetMap">
               <TileLayer
@@ -108,39 +113,47 @@ function App() {
           <LeafletgeoSearch/>
           <Geoman/>
         </MapContainer>
-        <div className="inline-flex rounded-md shadow-sm" role="group">
-          <button className="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white" onClick={showMyLocation}>
-            Locate Me
-          </button>
-          <button className="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white" onClick={handleGeomanData}>
+        <div className="inline-flex rounded-md shadow-sm"  role="group" style={{fontFamily: 'Open Sans, sans-serif',padding:'12px',marginTop:'15px',alignItems:"center",display: "flex",justifyContent: "space-between"}}>
+          
+          <button style={{color:"#fefae0",background:"#606c38",borderRadius:"19px",padding:"8px 29px"}} className="py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-black focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white" onClick={handleGeomanData}>
             Calculate
           </button>
+          <button style={{color:"#fefae0",background:"#606c38",borderRadius:"19px",padding:"8px 29px"}} className="py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-black focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white" onClick={showMyLocation}>
+            Locate Me
+          </button>
         </div>
+        </div>
+        
+        
       </div>
-      <div className="max-w-5xl mx-auto">
-        {nhDistInKm.length === 0?"":<div><p>Total Distance of NH to be constructed: {`${nhDistInKm.reduce((acc,el)=>acc+=el,0).toFixed(3)}`}km</p></div>}
-        {shDistInKm.length === 0?"":<div><p>Total Distance of SH to be constructed: {`${shDistInKm.reduce((acc,el)=>acc+=el,0).toFixed(3)}`}km</p></div>}
-        {rrDistInKm.length === 0?"":<div><p>Total Distance of RR to be constructed: {`${rrDistInKm.reduce((acc,el)=>acc+=el,0).toFixed(3)}`}km</p></div>}
+      <div className="max-w-5xl mx-auto" style={{fontFamily: 'Open Sans, sans-serif',fontSize:"30px",marginTop:"25px"}}>
+        {nhDistInKm.length === 0?"":<div><p>Total Distance of NH to be constructed: <i style={{color:'#6c584c'}}>{`${nhDistInKm.reduce((acc,el)=>acc+=el,0).toFixed(3)}`} km</i></p></div>}
+        {shDistInKm.length === 0?"":<div><p>Total Distance of SH to be constructed: <i style={{color:'#6c584c'}}>{`${shDistInKm.reduce((acc,el)=>acc+=el,0).toFixed(3)}`} km</i></p></div>}
+        {rrDistInKm.length === 0?"":<div><p>Total Distance of RR to be constructed: <i style={{color:'#6c584c'}}>{`${rrDistInKm.reduce((acc,el)=>acc+=el,0).toFixed(3)}`} km</i></p></div>}
       </div>
-      <div className="max-w-5xl mx-auto">
-        {nhAreaInM.length === 0?"":<div><p>Total Area of trees to be cut for contruction of NH: {`${(nhAreaInM.reduce((acc,el)=>acc+=el,0)/1000000).toFixed(3)}`}km&sup2;</p></div>}
-        {shAreaInM.length === 0?"":<div><p>Total Area of trees to be cut for contruction of SH: {`${(shAreaInM.reduce((acc,el)=>acc+=el,0)/1000000).toFixed(3)}`}km&sup2;</p></div>}
-        {rrAreaInM.length === 0?"":<div><p>Total Area of trees to be cut for contruction of RR: {`${(rrAreaInM.reduce((acc,el)=>acc+=el,0)/1000000).toFixed(3)}`}km&sup2;</p></div>}
+      <div className="max-w-5xl mx-auto" style={{fontFamily: 'Open Sans, sans-serif',fontSize:"30px",marginTop:"25px"}}>
+        {nhAreaInM.length === 0?"":<div><p>Total Area of trees to be cut for contruction of NH: <i style={{color:'#6c584c'}}>{`${(nhAreaInM.reduce((acc,el)=>acc+=el,0)/1000000).toFixed(3)}`} km&sup2;</i></p></div>}
+        {shAreaInM.length === 0?"":<div><p>Total Area of trees to be cut for contruction of SH: <i style={{color:'#6c584c'}}>{`${(shAreaInM.reduce((acc,el)=>acc+=el,0)/1000000).toFixed(3)}`} km&sup2;</i></p></div>}
+        {rrAreaInM.length === 0?"":<div><p>Total Area of trees to be cut for contruction of RR: <i style={{color:'#6c584c'}}>{`${(rrAreaInM.reduce((acc,el)=>acc+=el,0)/1000000).toFixed(3)}`} km&sup2;</i></p></div>}
       </div>
-      <div className="max-w-5xl mx-auto">
-        {nhAreaInM.length !== 0 || shAreaInM.length!==0 || rrAreaInM.length!==0?<div><p>Total Emission from Trees Cut: {`${(((nhAreaInM.reduce((acc,el)=>acc+=el,0)+shAreaInM.reduce((acc,el)=>acc+=el,0), rrAreaInM.reduce((acc,el)=>acc+=el,0))/1000000).toFixed(3))*121,405.8}`}tCO<sub>2</sub></p>  </div>:""}
+      <div className="max-w-5xl mx-auto" style={{fontFamily: 'Open Sans, sans-serif',fontSize:"30px",marginTop:"25px"}}>
+        {nhAreaInM.length !== 0 || shAreaInM.length!==0 || rrAreaInM.length!==0?<div><p>Total Emission from Trees Cut:<i style={{color:'#6c584c'}}> {`${(((nhAreaInM.reduce((acc,el)=>acc+=el,0)+shAreaInM.reduce((acc,el)=>acc+=el,0), rrAreaInM.reduce((acc,el)=>acc+=el,0))/1000000).toFixed(3))*121,405.8}`} tCO<sub>2</sub></i></p> </div>:""}
       </div>
-      <div className="flex justify-center">
-          {nhDistInKm.length === 0?"":<PieChart chartData={road_material[0].Carbon_emission} name="% of Materials Used in Construction of NH" type="volume"/>}
-          {shDistInKm.length === 0?"":<PieChart chartData={road_material[1].Carbon_emission} name="% of Materials Used in Construction of SH" type="volume"/>}
-          {rrDistInKm.length === 0?"":<PieChart chartData={road_material[2].Carbon_emission} name="% of Materials Used in Construction of RR" type="volume"/>}
+      
+      <div style={{display:"flex",justifyContent:"space-around"}}>
+          <div className="flex justify-center" style={{fontFamily: 'Open Sans, sans-serif',marginTop:"25px"}}>
+              {nhDistInKm.length === 0?"":<PieChart chartData={road_material[0].Carbon_emission} name="% of Materials Used in Construction of NH" type="volume"/>}
+              {shDistInKm.length === 0?"":<PieChart chartData={road_material[1].Carbon_emission} name="% of Materials Used in Construction of SH" type="volume"/>}
+              {rrDistInKm.length === 0?"":<PieChart chartData={road_material[2].Carbon_emission} name="% of Materials Used in Construction of RR" type="volume"/>}
+          </div>
+          <div className="flex justify-center" style={{fontFamily: 'Open Sans, sans-serif',marginTop:"25px"}}>
+              {nhDistInKm.length === 0?"":<PieChart chartData={road_material[0].Carbon_emission} name="% of CO2 Emission by Materials Used in Construction of NH" type="emission"/>}
+              {shDistInKm.length === 0?"":<PieChart chartData={road_material[1].Carbon_emission} name="% of CO2 Emission by Materials Used in Construction of SH" type="emission"/>}
+              {rrDistInKm.length === 0?"":<PieChart chartData={road_material[2].Carbon_emission} name="% of CO2 Emission by Materials Used in Construction of RR" type="emission"/>}
+          </div>
       </div>
-      <div className="flex justify-center">
-          {nhDistInKm.length === 0?"":<PieChart chartData={road_material[0].Carbon_emission} name="% of CO2 Emission by Materials Used in Construction of NH" type="emission"/>}
-          {shDistInKm.length === 0?"":<PieChart chartData={road_material[1].Carbon_emission} name="% of CO2 Emission by Materials Used in Construction of SH" type="emission"/>}
-          {rrDistInKm.length === 0?"":<PieChart chartData={road_material[2].Carbon_emission} name="% of CO2 Emission by Materials Used in Construction of RR" type="emission"/>}
-      </div>
-      <div className="tabularDetails">
+      
+      <div className="tabularDetails" >
           {nhDistInKm.length===0 ?"":<TabularData tableData={road_material[0].Carbon_emission} multiplier={nhDistInKm.reduce((acc,el)=>acc+=el,0)} name="Construction of NH Details:"/>}
           {shDistInKm.length===0 ?"":<TabularData tableData={road_material[1].Carbon_emission} multiplier={shDistInKm.reduce((acc,el)=>acc+=el,0)} name="Construction of SH Details:"/>}
           {rrDistInKm.length===0 ?"":<TabularData tableData={road_material[2].Carbon_emission} multiplier={rrDistInKm.reduce((acc,el)=>acc+=el,0)} name="Construction of RR Details:"/>}
